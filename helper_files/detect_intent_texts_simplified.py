c60feedc66e6cc72b5fb4135d2cc40dc10b4cdb7
eric-dl <31330964+eric-dl@users.noreply.github.com>
2018-02-21 07:08:46 -0600

    playing with context

--- helper_files/detect_intent_texts_simplified.py
+++ helper_files/detect_intent_texts_simplified.py
@@ -7,7 +7,7 @@ import dialogflow
 project_id = "fmla-faq"
 language_code = "en-US"
 session_id = str(uuid.uuid4())
-text = "What is FMLA?"
+text = "thisisarandomtestforeric"
 
 # [START dialogflow_detect_intent_text]
 def detect_intent_texts():
@@ -20,6 +20,12 @@ def detect_intent_texts():
 
     query_input = dialogflow.types.QueryInput(text=text_input)
 
+    client_context = dialogflow.ContextsClient()
+#    context = client_context.get_context("projects/fmla-faq/agent/sessions/b805e39f-3d28-4222-b159-cb46776ef1dc/contexts/randomtestforeric-followup")
+#    dialogflow.types.Context(name="projects/fmla-faq/agent/sessions/e8704b67-400f-4a90-bdfb-6b58db4bf3b4/contexts/randomtestforeric-followup", lifespan_count=1, parameters = "{fields {key: \"date\" value {string_value: ""}}fields {key: \"date.original\" value {string_value: \"\"}} fields { key: \"geo-city\" value { string_value: \"\" } } fields { key: \"geo-city.original\" value { string_value: \"\" } } fields { key: \"geo-state\" value { string_value: \"\" } } fields { key: \"geo-state.original\" value { string_value: \"\" } } } ")
+
+#    query_params = dialogflow.types.QueryParameters(contexts=context)
+
     response = session_client.detect_intent(
         session=session, query_input=query_input)
 
@@ -30,6 +36,15 @@ def detect_intent_texts():
         response.query_result.intent_detection_confidence))
     print('Fulfillment text: {}\n'.format(
         response.query_result.fulfillment_text))
+    print('Parameter text: {}\n'.format(
+        response.query_result.parameters))
+    print('Output context: {}\n'.format(
+        response.query_result.output_contexts))
+    print("***********************************")
+    print(response.query_result.output_contexts.pop())
+
+#    context = client_context.get_context(response.query_result.output_contexts.parameters)
+#    print(context.lifespan_count)
 
 
 detect_intent_texts()
